610d1e8149e1f1c36d69d3de5cf14376097ef031
Brett Samson <brettsamson@gmail.com>
2017-01-02 12:07:08 -0600

    Fix cli so it doesn't crash.

--- ansible/roles/mtpereira.passenger/tests/apache/config.ru
+++
@@ -1,4 +0,0 @@
-app = proc do |env|
-    [200, { "Content-Type" => "text/html" }, ["hello world"]]
-end
-run app
--- lib/subspace/railtie.rb
+++
@@ -1,9 +0,0 @@
-module Subspace
-  class Railtie < ::Rails::Railtie
-    railtie_name :subspace
-
-    rake_tasks do
-      Dir[File.join(File.dirname(__FILE__),'../tasks/*.rake')].each { |f| load f }
-    end
-  end
-end
