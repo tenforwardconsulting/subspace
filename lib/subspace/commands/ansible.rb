cf91afb3a3150722bd446e1d657c8e2baf89269f
Zach Boatrite <zach@tenforwardconsulting.com>
2017-04-13 11:10:10 -0500

    Update ansible.cfg when any ansible command is ran, not just provision

--- lib/subspace/commands/ansible.rb
+++ lib/subspace/commands/ansible.rb
@@ -2,11 +2,18 @@ module Subspace
   module Commands
     module Ansible
       def ansible_command(command, *args)
+        update_ansible_cfg
         Dir.chdir "config/provision" do
           say ">> Running #{command} #{args.join(' ')}"
           system(command, *args, out: $stdout, err: $stderr)
           say "<< Done"
         end
+      end
+
+      private
+
+      def update_ansible_cfg
+        template "ansible.cfg"
       end
     end
   end
