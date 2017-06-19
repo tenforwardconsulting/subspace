1803e4bd6e0a8b9a61985522e61514ff6d9f97cb
Brian Samson <brian@briansamson.com>
2017-06-18 22:29:40 -0500

    update nginx -- still needs letsencrypt

--- ansible/roles/puma/templates/puma.rb
+++ ansible/roles/puma/templates/puma.rb
@@ -3,23 +3,24 @@ workers Integer(ENV['WEB_CONCURRENCY'] || 4)
 
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 # Min and Max threads per worker
-threads threads_count, threads_count
+threads 0, threads_count
 
-app_dir = File.expand_path("../..", __FILE__)
+app_dir = "/u/apps/{{project_name}}/current"
+directory app_dir
 
-# Default to production
-rails_env = ENV['RAILS_ENV'] || "production"
+rails_env = "{{rails_env}}"
 environment rails_env
 
 # Set up socket location
-bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+bind "tcp://127.0.0.1:9292"
 
 # Logging
 stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{app_dir}/tmp/pids/puma.pid"
-state_path "#{app_dir}/tmp/pids/puma.state"
+daemonize
+pidfile "/u/apps/{{project_name}}/shared/tmp/pids/puma.pid"
+state_path "/u/apps/{{project_name}}/shared/tmp/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
@@ -27,3 +28,6 @@ on_worker_boot do
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
   ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
 end
+
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
